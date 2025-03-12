c3bfcf188e2807dd294ef3d573f314f2f3930691
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:13:02 +0700

    menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ConsoleApp1;
+using modul4_103022330044;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Pilih menu:");
+                Console.WriteLine("1. KodeProduk");
+                Console.WriteLine("2. x");
+                Console.WriteLine("3. Keluar");
+                Console.Write("Masukkan pilihan Anda: ");
+                string pilihan = Console.ReadLine();
+
+                switch (pilihan)
+                {
+                    case "1":
+                        HandleKodeProduk();
+                        break;
+                    case "2":
+                        //HandleDoorMachine();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Pilihan tidak valid. Silakan coba lagi.");
+                        break;
+                }
+            }
+        }
+
+        static void HandleKodeProduk()
+        {
+            KodeProduk kodeProduk = new KodeProduk();
+            Console.Write("Masukkan nama produk: ");
+            string produk = Console.ReadLine();
+            string kode = kodeProduk.GetKodeProduk(produk);
+            Console.WriteLine($"Kode pos untuk {produk} adalah {kode}");
+        }
+        
+    }
+}
